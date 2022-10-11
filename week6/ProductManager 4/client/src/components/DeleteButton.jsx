6b2bbb081bce336e777cec98fcb14c7cef779986
weird10 <ryandavidtan10@gmail.com>
2022-10-11 12:48:54 +0700

    added PetShelter

--- week6/ProductManager 4/client/src/components/DeleteButton.jsx
+++ week6/ProductManager 4/client/src/components/DeleteButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const DeleteButton = (props) => {
     const { productId, successCallback } = props
--- week6/ProductManager 4/client/src/components/Detail.jsx
+++ week6/ProductManager 4/client/src/components/Detail.jsx
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import {useNavigate, useParams} from "react-router-dom";
+import DeleteButton from './DeleteButton';
 
 
 const Detail = (props) => {
     const [thisProduct, setThisProduct] = useState({});
     const {id} = useParams();
     const navigate = useNavigate();
+
 
     
     useEffect (() => {
@@ -17,24 +19,14 @@ const Detail = (props) => {
         }).catch(err=> console.log(err))
     } , [id])
 
-    const deleteHandler = () => {
-        axios.delete(`http://localhost:8000/api/product/${id}`)
-            .then((res) => {
-                console.log(res);
-                console.log(res.data);
-                navigate("/products");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
 
   return (
     <div>
         <h2>{thisProduct.title}</h2>
         <p>Price: ${thisProduct.price}</p>
         <p>Description: {thisProduct.description}</p>
-        <button onClick={deleteHandler}>Delete</button>
+        <DeleteButton productId={thisProduct._id} successCallBack />
+
     </div>
   )
 }
